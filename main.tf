546700dd106c1a93d0e1666e9daf7009ea1ffa9d
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-11-07 09:25:53 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,21 @@ resource "vaultoperator_init" "default" {
   recovery_threshold = 3
 }
   
-resource "tfe_workspace" "test" {
-  name         = "my-workspace-name"
-  organization = "captain-antonio-051820"
+  data "tfe_organizations" "org" {}
+  
+resource "tfe_variable_set" "tf_core" {
+  name         = "Global Varset"
+  description  = "Variable set applied to all workspaces."
+  global       = true
+  organization = tfe_organizations.org.names[0]
+}
+  
+  
+resource "tfe_variable" "test-a" {
+  key             = "VAULT_ADDR"
+  value           = var.VAULT_ADDR
+  category        = "env"
+  sensitive = false
+  description     = "Vault Server Address"
+  variable_set_id = tfe_variable_set.tf_core.id
 }
